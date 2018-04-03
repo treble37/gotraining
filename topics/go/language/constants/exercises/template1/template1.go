215a129e090ff8572d939459c71f06f5c4434365
Bruce Park <brucepark0@gmail.com>
2018-04-03 12:00:29 -0700

    Solution to constants exercises

--- topics/go/language/constants/exercises/template1/template1.go
+++ topics/go/language/constants/exercises/template1/template1.go
@@ -7,19 +7,24 @@
 package main
 
 // Add imports.
+import "fmt"
 
 const (
-// Declare a constant named server of kind string and assign a value.
-
-// Declare a constant named port of type integer and assign a value.
+	// Declare a constant named server of kind string and assign a value.
+	server = "Phusion"
+	// Declare a constant named port of type integer and assign a value.
+	port = 32
 )
 
 func main() {
 
 	// Display the value of both server and port.
-
+	fmt.Println("server: ", server)
+	fmt.Println("port: ", port)
 	// Divide a constant of kind integer and kind floating point and
 	// assign the result to a variable.
+	v := 45 / 2.0
 
 	// Display the value of the variable.
+	fmt.Println(v)
 }
