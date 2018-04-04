4a3d27c6276e2a159fabc6d18f54ff0f48c1c105
Bruce Park <brucepark0@gmail.com>
2018-04-04 10:46:28 -0700

    Add training_notes day 2 and methods solution

--- topics/go/language/methods/exercises/template1/template1.go
+++ topics/go/language/methods/exercises/template1/template1.go
@@ -8,18 +8,44 @@
 package main
 
 // Add imports.
+import (
+	"fmt"
+	"math/rand"
+)
 
 // Declare a struct that represents a ball player.
 // Include field called name, atBats and hits.
+type player struct {
+	name   string
+	atBats int
+	hits   int
+}
 
 // Declare a method that calculates the batting average for a player.
-func ( /* receiver */ ) average() /* return type */ {
+func (p *player) average() float64 {
+	if p.atBats == 0.0 {
+		return 0.0
+	}
+	return float64(p.hits) / float64(p.atBats)
 }
 
 func main() {
 
 	// Create a slice of players and populate each player
 	// with field values.
-
+	players := make([]player, 2)
+	players[0] = player{
+		name:   "John0",
+		atBats: rand.Intn(100),
+		hits:   rand.Intn(100),
+	}
+	players[1] = player{
+		name:   "John1",
+		atBats: rand.Intn(100),
+		hits:   rand.Intn(100),
+	}
 	// Display the batting average for each player in the slice.
+	for _, player := range players {
+		fmt.Printf("hits[%d] atBats[%d] average[%f]\n", player.hits, player.atBats, player.average())
+	}
 }
--- training_notes_day2.md
+++ training_notes_day2.md
@@ -8,3 +8,15 @@
 u, ok := users["Roy"]
 //ok will be true if the value exists there
 ```
+
+### Methods
+
+```
+func (u user) notify() {}
+```
+
+* "(u user)" is the method receiver
+* You can add methods to any type you create (but not to types you didn't create)
+* If you need to mutate bill in topics/go/language/methods/example1/example1.go, you should use pointers
+* General advice: if you need to mutate something on a type, provide a pointer/receiver method (this advice isn't always true, but generally is)
+* Look at the nature of your type. Is that thing something that should be copied?
