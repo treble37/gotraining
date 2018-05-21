7e110099c6602ddec98403ee73e22eba7a401ef7
Bruce <brucepark0@gmail.com>
2018-05-20 18:08:39 -0700

    Do goroutine exercise

--- topics/go/concurrency/goroutines/exercises/template1/template1.go
+++ topics/go/concurrency/goroutines/exercises/template1/template1.go
@@ -10,7 +10,11 @@
 package main
 
 // Add imports.
-import "runtime"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
 func init() {
 
@@ -21,24 +25,33 @@ func init() {
 func main() {
 
 	// Declare a wait group and set the count to two.
-
+	var wg sync.WaitGroup
+	wg.Add(2)
 	// Declare an anonymous function and create a goroutine.
-	{
+	go func() {
 		// Declare a loop that counts down from 100 to 0 and
 		// display each value.
-
+		for i := 100; i >= 0; i-- {
+			fmt.Println(i)
+		}
 		// Tell main we are done.
-	}
+		wg.Done()
+	}()
 
 	// Declare an anonymous function and create a goroutine.
-	{
+	go func() {
 		// Declare a loop that counts up from 0 to 100 and
 		// display each value.
+		for i := 0; i <= 100; i++ {
+			fmt.Println(i)
+		}
 
 		// Tell main we are done.
-	}
+		wg.Done()
+	}()
 
 	// Wait for the goroutines to finish.
-
+	wg.Wait()
 	// Display "Terminating Program".
+	fmt.Println("Terminating Program")
 }
